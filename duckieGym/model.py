8890264d9c9787ecf96d68f41a5cd5e069f511ef
Schneider Marcell <78796219+Marci0707@users.noreply.github.com>
2021-11-28 00:40:50 +0100

    tried tuning hyperparams, some small quality of life changes. Added evaluation after training

--- duckieGym/model.py
+++ duckieGym/model.py
@@ -1,49 +1,64 @@
+from keras import regularizers
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, LeakyReLU, BatchNormalization, MaxPooling2D, Flatten, Dense
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, LearningRateScheduler, ReduceLROnPlateau
 from tensorflow.keras.optimizers import Adam
 import tensorflow as tf
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
 
 from os import listdir
 
 import numpy as np
+from tensorflow.python.keras.layers import Dropout
 
 from duckieGym.LoggerCallback import LoggerCallback
 
 
 def scheduler(epoch):
-
-    if epoch < 15:
-        return 1.0
     if epoch < 30:
-        return 0.1
+        return 0.01
+    if epoch < 45:
+        return 0.005
     else:
-        return 0.05
-    if epoch < 20:
-        return 0.1
-    if epoch < 40:
-        return 0.020013423
-    if epoch < 75:
-        return 0.0032421343
-    else:
-        return 0.0004
+        return 0.001
+
+
+def scale(X, Y):
+    X = X / 255.0
+
+    velocity_steer_scaler = StandardScaler()
+    Y = velocity_steer_scaler.fit_transform(Y)
+
+    return (X, Y), velocity_steer_scaler
 
 
 def read_image(filename):
     image = tf.keras.preprocessing.image.load_img(filename)
     input_arr = tf.keras.preprocessing.image.img_to_array(image)
-    for i in range(3):
-        scaler = MinMaxScaler().fit(input_arr[:, :, i])
-        input_arr[:, :, i] = scaler.transform(input_arr[:, :, i])
 
     return input_arr
 
 
-def read_data(images_dir_name= "preprocessedImages", label_file="my_app.txt"):
+def split_data(X, Y, ratio=(0.7, 0.15, 0.15)):
+    train_split = ratio[0]
+    valid_split = ratio[1]
+    test_split = ratio[2]
+    nb_samples = X.shape[0]
+
+    X_train = X[0:int(nb_samples * train_split)]
+    Y_train = Y[0:int(nb_samples * train_split)]
+    X_valid = X[int(nb_samples * train_split):int(nb_samples * (1 - test_split))]
+    Y_valid = Y[int(nb_samples * train_split):int(nb_samples * (1 - test_split))]
+    X_test = X[int(nb_samples * (1 - test_split)):]
+    Y_test = Y[int(nb_samples * (1 - test_split)):]
+
+    return (X_train, Y_train), (X_valid, Y_valid), (X_test, Y_test)
+
+
+def read_data(images_dir_name="preprocessedImages", label_file="my_app.txt"):
     img_list = np.zeros((len(listdir(images_dir_name)), 48, 85, 3))
     for i, f in enumerate(listdir(images_dir_name)):
-        img_list[i] = (read_image(images_dir_name+ "/" + f))
+        img_list[i] = (read_image(images_dir_name + "/" + f))
 
     X = np.asarray(img_list, dtype='float32')
 
@@ -55,20 +70,7 @@ def read_data(images_dir_name= "preprocessedImages", label_file="my_app.txt"):
             y = np.asarray(y, dtype="float32")
             Y[i] = y
 
-
-    train_split=0.7
-    valid_split=0.15
-    test_split=0.15
-    nb_samples=X.shape[0]
-
-    X_train = X[0:int(nb_samples*(train_split))]
-    Y_train = Y[0:int(nb_samples*(train_split))]
-    X_valid = X[int(nb_samples*(train_split)):int(nb_samples*(1-test_split))]
-    Y_valid = Y[int(nb_samples*(train_split)):int(nb_samples*(1-test_split))]
-    X_test = X[int(nb_samples*(1-test_split)):]
-    Y_test = Y[int(nb_samples*(1-test_split)):]
-
-    return (X_train,Y_train,X_valid,Y_valid,X_test,Y_test)
+    return X, Y
 
 
 def create_model(input_shape):
@@ -90,33 +92,45 @@ def create_model(input_shape):
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
     model.add(Flatten())
+    model.add(Dropout(0.15))
 
-    model.add(Dense(12800, activation="relu"))
+    model.add(Dense(12000, activation="relu", kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4)))
+    model.add(Dropout(0.2))
     model.add(Dense(2, activation="linear"))
 
     return model
 
 
-def run_model(model,X_train,Y_train,X_valid,Y_valid):
-
-
-    early_stopping = EarlyStopping(patience=20, verbose=1, monitor='loss', mode='min')
-    reduce_lr = ReduceLROnPlateau(monitor='loss', factor=0.8, patience=20, min_lr=10e-5)
+def train_model(model, X_train, Y_train, X_valid, Y_valid):
+    early_stopping = EarlyStopping(patience=10, verbose=1, monitor='val_loss', mode='min')
+    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.8, patience=20, min_lr=10e-5)
     checkpoint = ModelCheckpoint(filepath='duckie.hdf5', verbose=1, save_best_only=True)
     change_lr = LearningRateScheduler(scheduler)
 
-
     model.compile(loss='mse',
                   optimizer=Adam(),
-                  metrics=['mse'])
+                  metrics=["mse"])
 
     print(model.summary())
 
-    model.fit(X_train, Y_train, batch_size=32, epochs=10000,validation_data=(X_valid,Y_valid),
-              callbacks=[early_stopping, reduce_lr, checkpoint, change_lr, LoggerCallback("stat.csv")], verbose=1, shuffle=True)
+    model.fit(X_train, Y_train, batch_size=32, epochs=10000, validation_data=(X_valid, Y_valid),
+              callbacks=[early_stopping, reduce_lr, checkpoint, change_lr, LoggerCallback("stat.csv")], verbose=1,
+              shuffle=True)
 
 
-X_train,Y_train,X_valid,Y_valid,X_test,Y_test = read_data() 
+X, Y = read_data()
+
+(X_scaled, Y_scaled), velocity_steering_scaler = scale(X, Y)
+
+(X_train, Y_train), (X_valid, Y_valid), (X_test, Y_test) = split_data(X_scaled, Y_scaled)
+
 model = create_model(X_train[0].shape)
-run_model(model,X_train,Y_train,X_valid,Y_valid)
-model.evaluate(X_test,Y_test,batch_size=32)
+
+train_model(model, X_train, Y_train, X_valid, Y_valid)
+
+print("eval score: ", model.evaluate(X_test, Y_test, batch_size=32))
+
+y_test_pred = model.predict(X_test)
+
+for idx, pred in enumerate(y_test_pred): #TODO maybe pyplot history after training
+    print(pred, Y_test[idx])
