af11e931b75b49a7c21053603a76054befbab187
Marci0707 <Negotation07>
2021-12-14 20:32:24 +0100

    model evaluation

--- duckieGym/log_util.py
+++ duckieGym/log_util.py
@@ -45,9 +45,11 @@ class Logger:
 
     def save_image_and_labels(self, episode):
 
-        original_images_dir = "originalImages"
-        preprocessed_images_dir = "preprocessedImages"
-        with open("my_app.txt", "a") as file:
+        map_name = "small_loop"
+        original_images_dir = os.path.join(os.getcwd(),"test_set","originalImages")
+        preprocessed_images_dir = os.path.join(os.getcwd(),"test_set",map_name)
+        label_file = os.path.join(os.getcwd(),"test_set",map_name+".txt")
+        with open(label_file, "a") as file:
             for step in episode.steps:
                 t = str(round(time.time() * 1000))  # using it as frame name
                 original_img_path = os.path.join(os.getcwd(), original_images_dir, t + ".png")
@@ -57,8 +59,8 @@ class Logger:
                 steer, velocity = step.action
                 file.write(t + " " + str(steer) + " " + str(velocity) + "\n")
 
-                og_img = Image.fromarray(img_array)
-                og_img.save(original_img_path)
+                #og_img = Image.fromarray(img_array)
+                #og_img.save(original_img_path)
 
                 preprocessed_img = preprocess_image(img_array)
                 preprocessed_img.save(preprocessed_img_path)
